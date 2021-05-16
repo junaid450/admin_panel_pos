bebaa703e26f0ee4c204e53a7580f2255cefcc30
junaid450 <84170948+junaid450@users.noreply.github.com>
2021-05-16 18:53:29 +0500

    Add files via upload

---
+++ login/login1.php
@@ -0,0 +1,67 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css"
+        integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    <link rel="stylesheet" href="css1.css">
+    <title>Hello, world!</title>
+</head>
+
+<body style="background: url('bg_image.jpg') no-repeat center center fixed  !important; ">
+
+
+    <div class="container h-100">
+        <div class="d-flex justify-content-center h-100">
+            <div class="user_card" style="background: #f5f5f5;">
+
+                <div>
+                    <h3 class="mt-1 md-1" style="text-align: center;">Login</h3>
+                </div>
+
+
+                <form action="" autocomplete="off" class="mt-5">
+                    <div class="form-group ">
+                        <!-- <label for="form-control">First name:</label> -->
+                        <input type="text" class="form-control" name="username" placeholder="Username">
+                    </div>
+                    <div class="form-group">
+                        <!-- <label for="form-control">Password:</label> -->
+                        <input type="password" class="form-control" name="password" placeholder="Password">
+                    </div>
+
+                    <div style="text-align: center;" class="mb-3">
+                        <a href="index.php"><button type="button" id="sendlogin"
+                                class="btn btn-primary rounded-pill px-4" style="text-align: center;">Login</button></a>
+                    </div>
+
+                </form>
+
+                <a href="#" style="text-align: center;">
+                            <p>Forgot Password?</p>
+                        </a>
+
+            </div>
+        </div>
+
+
+
+
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"
+            integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous">
+        </script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js"
+            integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous">
+        </script>
+
+</body>
+
+</html>
